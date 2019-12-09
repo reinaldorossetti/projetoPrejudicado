9773e51fccf24e8266eb9c1c2c96e3e82f2b91a2
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2019-12-09 18:00:44 -0300

    Update acessoSelenium.py

--- acessoSelenium.py
+++ acessoSelenium.py
@@ -1,65 +1,24 @@
-"""
-Professor,
-
-Quando estou no meu computador normal, logado com a senha, o link abre direto:
-http://www.receita.fazenda.gov.br/PessoaJuridica/CNPJ/fcpj/consulta.asp
-
-Porém ao abrir com driver...pede a senha toda vez e abre o recaptcha. O recaptcha detecta robo e me trava o código.
-Não consigo passar o recaptcha.
-
-O senhor pode acessar o link acima, informar o cpf '01303891026', e a senha: 'ZIP/DANI' e digitar esse protocolo expirado por exemplo "RSP1900282334"
-Depois faça com meu script, verá que o site direciona para logar...sem ir direto para onde deveria ir, fazendo que eu chame duas vezes a mesma url.
-
-
-Nota: eu uso (By.ID, ''), ESSE meu script é antigo pq eu desisti de tentar uma solução.
-"""
-
-
-import pdb
+# -*- coding: utf-8 -*-
 from time import sleep
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-import os
 
+dir = 'C:\Users\ReinaldoRossettii\AppData\Roaming\Mozilla\Firefox\Profiles\878rtczp.reinaldo'
+firefox_profile = webdriver.FirefoxProfile(profile_directory=dir)
+driver = webdriver.Firefox(firefox_profile=firefox_profile)
 
-def limpar_terminal():
-    os.system('clear')
-
-
-limpar_terminal()
-
-
-print("Instanciando Driver")
-driver = webdriver.Chrome()
 driver.maximize_window()
+driver.set_page_load_timeout(30)
+driver.implicitly_wait(60)
+driver.set_script_timeout(5)
 
-print('Acessando URL')
-driver.get(
-    "http://www.receita.fazenda.gov.br/PessoaJuridica/CNPJ/fcpj/consulta.asp")
-
-
-print('Fazendo Login')
-driver.find_element_by_id("j_username").clear()
-driver.find_element_by_id("j_username").send_keys("01303891026")
-driver.find_element_by_id("kc-login").click()
-
-driver.find_element_by_id("j_password").clear()
-driver.find_element_by_id("j_password").send_keys("ZIP/DANI")
-sleep(3)
-driver.find_element_by_id("submit-button").click()
-sleep(20)
-
-"""
-Por causa desse rediciocionar para logar, abaixo tenho que chamar novamente o link inicial:
-"""
-driver.get(
-    "http://www.receita.fazenda.gov.br/PessoaJuridica/CNPJ/fcpj/consulta.asp")
-
-
-sleep(2)
-driver.find_element_by_id("prot").send_keys("RSP1900282334")
-
-
+# carrega a pagina com a sessao salva no profile
+driver.get("https://areadousuario.redesim.gov.br/")
+# precisa de um tempo pra carregar os cookies, se for muito rapido ele te redireciona.
 sleep(5)
-print("Chrome fim.")
+# muda a pagina sem apagar os cookies
+driver.execute_script("window.location='http://www.receita.fazenda.gov.br/PessoaJuridica/CNPJ/fcpj/consulta.asp'")
+sleep(5)
+# inserindo os dados via javascript
+driver.execute_script('document.querySelector("#prot").value="RSP1900282334"')
+sleep(5)
 driver.close()
